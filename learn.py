0cb6f783fa09cf3e8c40de82a207328ba988255c
Masood <shaikmasood7815@gmail.com>
2025-12-02 15:32:48 +0530

    still learning

--- learn.py
+++ learn.py
@@ -16,7 +16,7 @@ y = input("enter name nat: ")
 nat = greet(y)
 print(nat)
 """
-
+'''
 secret = 9
 
 try:
@@ -30,3 +30,53 @@ try:
         print("you got it.")
 except ValueError:
     print("Please enter a valid number.")
+
+    '''
+'''
+num = int(input('enter numb:' ))
+while num > 0:
+    print("Hello")
+    num = num-1
+
+n = 0
+
+where = input("go left or right? ")
+while where != "right":
+    n = n + 1
+    where = input("go left or right? ")
+print("you got out!")
+'''
+'''
+mysum = 0
+start = 3
+end = 5
+for i in range(start, end):
+    mysum += i
+print(mysum)
+'''
+'''
+#finds factorial
+num = int(input("enter even if you want even or odd: "))
+for i in range(0, 20+1, 2):
+    print(i)
+'''
+'''
+# Budget calculation
+budgets = []
+categories = ['twenty', 'thirty', 'fourty', 'eight', 'two']
+
+for category in categories:
+    value = float(input(f'Enter budget for {category}: '))
+    budgets.append(value)
+
+total_budget = sum(budgets)
+average_budget = total_budget / 100
+print(f'Total budget is ${average_budget:,.2f}')
+'''
+
+x = 10
+y = 20
+
+while (x < y):
+    print("you're right")
+    x += 1
